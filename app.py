7f8ea1773043d4f8e10ce288bcc5c1a63dcb4734
Vishal Soni <84185041+vishalsnitk@users.noreply.github.com>
2024-07-24 12:10:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,125 @@
+from flask import Flask, render_template, request
+
+import os
+
+from data_processing import (
+    load_data, filter_data, create_memory_usage_plot, create_assembly_state_plot, 
+    create_timer_state_plot, 
+    generate_stepwise_memory_usage_plots, generate_stepwise_assembly_state_plots, 
+    generate_stepwise_timer_state_plots, 
+    generate_memory_usage_line_plot, generate_assembly_state_line_plot,
+    generate_timer_state_line_plot,
+    convert_bytes_to_mb
+)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    folder_path = os.path.join(app.root_path, 'static', 'PES188858')
+    # print(folder_path)
+    df = load_data(folder_path)
+    baselines = df['Baseline'].unique()
+    steps = df['StepId'].unique()
+    selected_baselines = request.form.getlist('baselines')
+    
+
+    filtered_df = filter_data(df, selected_baselines)
+
+    plot_memory_usage = create_memory_usage_plot(filtered_df)
+    plot_assembly_state = create_assembly_state_plot(filtered_df)
+    plot_timer_state = create_timer_state_plot(filtered_df)
+    # plot_network_state = create_network_state_plot(filtered_df)
+
+    return render_template('home.html', 
+                           baselines=baselines,
+                           steps=steps,
+                           selected_baselines=selected_baselines,
+                           plot_memory_usage=plot_memory_usage, 
+                           plot_assembly_state=plot_assembly_state, 
+                           plot_timer_state=plot_timer_state)
+
+@app.route('/plot/<plot_type>')
+def plot(plot_type):
+    folder_path = os.path.join(app.root_path, 'static', 'PES188858')
+    df = load_data(folder_path)
+
+    if plot_type == 'memory_usage':
+        plot_img = create_memory_usage_plot(df)
+        title = 'Memory Usage by Baseline and Step'
+    elif plot_type == 'assembly_state':
+        plot_img = create_assembly_state_plot(df)
+        title = 'Assembly State by Baseline and Step'
+    elif plot_type == 'timer_state':
+        plot_img = create_timer_state_plot(df)
+        title = 'Timer State by Baseline and Step'
+    # elif plot_type == 'network_state':
+    #     plot_img = create_network_state_plot(df)
+    #     title = 'Network State by Baseline and Step'
+    else:
+        return "Plot type not found", 404
+
+    return render_template('plot.html', plot_img=plot_img, title=title)
+
+
+@app.route('/stepwise_analysis/<step_type>', methods=['GET'])
+def stepwise_analysis(step_type):
+    folder_path = os.path.join(app.root_path, 'static')
+    df = load_data(folder_path)
+
+    selected_steps = [step_type]
+    # print(selected_steps)
+    memory_plots = generate_stepwise_memory_usage_plots(df, selected_steps)
+    assembly_plots = generate_stepwise_assembly_state_plots(df, selected_steps)
+    timer_plots = generate_stepwise_timer_state_plots(df, selected_steps)
+    # network_plots = generate_stepwise_network_state_plots(df, selected_steps)
+
+    # Combine all plots into a single dictionary
+    stepwise_plots = {
+        "Memory Usage": memory_plots,
+        "Assembly State": assembly_plots,
+        "Timer State": timer_plots,
+        # "Network State": network_plots
+    }
+
+    # Debug print statements
+    print(f"Step type: {step_type}")
+    for category, plots in stepwise_plots.items():
+        for step, plot in plots.items():
+            print(f"Category: {category}, Step: {step}, Plot: {plot[:30]}...")
+
+    return render_template('stepwise_analysis.html', 
+                           step_type=step_type,
+                           stepwise_plots=stepwise_plots)
+
+@app.route('/baseline_analysis/<baseline>', methods=['GET'])
+def baseline_analysis(baseline):
+    folder_path = os.path.join(app.root_path, 'static')
+    df = load_data(folder_path)
+    baseline_df = df[df['Baseline'] == baseline]
+
+    # Convert memory-related columns to MB
+    memory_columns = [
+         'BytesInUse', 'MaxBytesInUse', 'BytesFromOS'
+    ]
+    baseline_df = convert_bytes_to_mb(baseline_df, memory_columns)
+
+    # Create memory usage plot
+    memory_plot = generate_memory_usage_line_plot(baseline_df)
+
+    # Similarly create assembly state, timer state, and network state plots
+    assembly_plot = generate_assembly_state_line_plot(baseline_df)
+    timer_plot = generate_timer_state_line_plot(baseline_df)
+    # network_plot = generate_network_state_line_plot(baseline_df)
+
+    return render_template('baseline_analysis.html',
+                           baseline=baseline,
+                           memory_plot=memory_plot,
+                           assembly_plot=assembly_plot,
+                           timer_plot=timer_plot)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
---
+++ data_processing.py
@@ -0,0 +1,367 @@
+import os
+import json
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+
+
+# def load_data(folder_path):
+#     all_data = []
+
+#     for filename in os.listdir(folder_path):
+#         if filename.endswith('.json'):
+#             file_path = os.path.join(folder_path, filename)
+#             with open(file_path, 'r') as file:
+#                 data = json.load(file)
+#                 environment = data.get('Environment', {})
+#                 environment_label = environment.get('Environment', {}).get('Group', 'Unknown')
+#                 steps = data.get('StepDetails', [])
+#                 for step in steps:
+#                     step_id = step.get('StepId', '')
+#                     memory_state = step.get('MemoryState', {})
+#                     assembly_state = step.get('AssemblyState', {}).get('Assembly Structure Details', {})
+#                     timer_state = step.get('TimerState', {}).get('TimerStats', {})
+#                     # network_state = step.get('NetworkState', {}).get('NetworkCount', {})
+
+#                     all_data.append({
+#                         'Baseline': environment_label,
+#                         'StepId': step_id,
+#                         # 'BytesPerObject': memory_state.get('Bytes per Object', 0),
+#                         'BytesInUse': memory_state.get('BytesInUse', 0),
+#                         'MaxBytesInUse': memory_state.get('MaxBytesInUse', 0),
+#                         'BytesFromOS': memory_state.get('BytesFromOS', 0),
+#                         # 'BytesForRollbackInUse': memory_state.get('Bytes for Rollback in use', 0),
+#                         # 'OSBytesForRollback': memory_state.get('OS Bytes for Rollback', 0),
+#                         'Number of Fully Loaded Parts': assembly_state.get('NumFullyLoadedParts', 0),
+#                         'Number of Partially Loaded Parts': assembly_state.get('NumPartiallyLoadedParts', 0),
+#                         'Number of Minimally Loaded Parts': assembly_state.get('NumMinimallyLoadedParts', 0),
+#                         'Number of OD Parts': assembly_state.get('NumOODParts', 0),
+
+#                         'Time required to perform an operation': timer_state.get('Time (seconds)', 0),
+#                         # 'NumSoaCalls': network_state.get('NumSoaCalls', 0),
+#                         # 'NumPdiCalls': network_state.get('NumPdiCalls', 0),
+#                         # 'NumFccCalls': network_state.get('NumFccCalls', 0)
+#                     })
+
+#     df = pd.DataFrame(all_data)
+#     return df
+
+
+def load_data(folder_path):
+    all_data = []
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.json'):
+            file_path = os.path.join(folder_path, filename)
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                environment = data.get('Environment', {})
+                environment_label = environment.get('Environment', {}).get('Group', 'Unknown')
+                steps = data.get('StepDetails', [])
+                for step in steps:
+                    step_id = step.get('StepId', '')
+                    memory_state = step.get('MemoryState', {})
+                    assembly_state = step.get('AssemblyState', {}).get('StructureDetails', {})
+                    timer_state = step.get('TimerState', {}).get('TimerStats', {})
+
+                    all_data.append({
+                        'Baseline': environment_label,
+                        'StepId': step_id,
+                        'BytesInUse': memory_state.get('BytesInUse', 0),
+                        'MaxBytesInUse': memory_state.get('MaxBytesInUse', 0),
+                        'BytesFromOS': memory_state.get('BytesFromOS', 0),
+                        'Number of Fully Loaded Parts': assembly_state.get('NumFullyLoadedParts', 0),
+                        'Number of Partially Loaded Parts': assembly_state.get('NumPartiallyLoadedParts', 0),
+                        'Number of Minimally Loaded Parts': assembly_state.get('NumMinimallyLoadedParts', 0),
+                        'Number of OD Parts': assembly_state.get('NumOODParts', 0),
+                        'Time required to perform an operation': timer_state.get('Time (seconds)', 0),
+                    })
+
+    df = pd.DataFrame(all_data)
+    # print(df)
+     
+
+    return df
+
+
+
+def filter_data(df, baselines):
+    if not baselines:
+        return df
+    return df[df['Baseline'].isin(baselines)]
+
+
+# def create_plot(df, columns, ylabel, title):
+#     baselines = df['Baseline'].unique()
+#     steps = df['StepId'].unique()
+
+#     fig, ax = plt.subplots(figsize=(18, 8))
+#     bar_width = 0.15
+#     bar_spacing = 0.05
+#     group_spacing = 0.5
+#     group_width = bar_width * len(steps) + bar_spacing * (len(steps) - 1) + group_spacing
+#     group_positions = np.arange(len(baselines)) * group_width
+#     colors = plt.cm.tab10(np.linspace(0, 1, len(columns)))
+
+#     for i, step in enumerate(steps):
+#         step_data = df[df['StepId'] == step]
+#         bar_positions = group_positions + i * (bar_width + bar_spacing)
+#         bottoms = np.zeros(len(baselines))
+
+#         for j, column in enumerate(columns):
+#             ax.bar(bar_positions,
+#                    step_data[column],
+#                    bar_width,
+#                    bottom=bottoms,
+#                    color=colors[j],
+#                    label=f'{column}' if i == 0 else "")
+#             bottoms += step_data[column]
+
+#     ax.set_xlabel('Baseline')
+#     ax.set_ylabel(ylabel)
+#     ax.set_title(title)
+#     ax.set_xticks(group_positions + (group_width - group_spacing) / 2 - bar_width / 2)
+#     ax.set_xticklabels(baselines, rotation=45, ha='right')
+#     ax.legend(title=f'{ylabel} Metrics', bbox_to_anchor=(1.05, 1), loc='upper left')
+#     plt.tight_layout()
+
+#     buf = BytesIO()
+#     plt.savefig(buf, format='png')
+#     plt.close(fig)
+#     buf.seek(0)
+#     img_base64 = base64.b64encode(buf.read()).decode('utf-8')
+#     return img_base64
+def create_plot(df, columns, ylabel, title):
+    existing_columns = [col for col in columns if col in df.columns]
+    if not existing_columns:
+        return "No valid columns found for plotting."
+
+    baselines = df['Baseline'].unique()
+    steps = df['StepId'].unique()
+
+    fig, ax = plt.subplots(figsize=(18, 8))
+    bar_width = 0.15
+    bar_spacing = 0.05
+    group_spacing = 0.5
+    group_width = bar_width * len(steps) + bar_spacing * (len(steps) - 1) + group_spacing
+    group_positions = np.arange(len(baselines)) * group_width
+    colors = plt.cm.tab10(np.linspace(0, 1, len(existing_columns)))
+
+    for i, step in enumerate(steps):
+        step_data = df[df['StepId'] == step]
+        bar_positions = group_positions + i * (bar_width + bar_spacing)
+        bottoms = np.zeros(len(baselines))
+
+        for j, column in enumerate(existing_columns):
+            ax.bar(bar_positions,
+                   step_data[column],
+                   bar_width,
+                   bottom=bottoms,
+                   color=colors[j],
+                   label=f'{column}' if i == 0 else "")
+            bottoms += step_data[column]
+
+    ax.set_xlabel('Baseline')
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.set_xticks(group_positions + (group_width - group_spacing) / 2 - bar_width / 2)
+    ax.set_xticklabels(baselines, rotation=45, ha='right')
+    ax.legend(title=f'{ylabel} Metrics', bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.tight_layout()
+
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close(fig)
+    buf.seek(0)
+    img_base64 = base64.b64encode(buf.read()).decode('utf-8')
+    return img_base64
+
+
+def create_memory_usage_plot(df):
+    columns = [
+ 'BytesInUse', 'MaxBytesInUse', 'BytesFromOS',
+    ]
+    return create_plot(
+        df, columns, 'Memory Usage (bytes)',
+        'Clustered Stacked Bar Chart of Memory Usage by Baseline and Step')
+
+
+def create_assembly_state_plot(df):
+    columns = [
+        'Number of Fully Loaded Parts', 'Number of Partially Loaded Parts',
+        'Number of Minimally Loaded Parts', 'Number of OD Parts'
+    ]
+    return create_plot(
+        df, columns, 'Number of Parts',
+        'Clustered Stacked Bar Chart of Assembly State by Baseline and Step')
+
+
+def create_timer_state_plot(df):
+    columns = ['Time required to perform an operation']
+    return create_plot(
+        df, columns, 'Time (seconds)',
+        'Clustered Stacked Bar Chart of Timer State by Baseline and Step')
+
+
+# def create_network_state_plot(df):
+#     columns = ['NumSoaCalls', 'NumPdiCalls', 'NumFccCalls']
+#     return create_plot(
+#         df, columns, 'Number of Calls',
+#         'Clustered Stacked Bar Chart of Network State by Baseline and Step')
+
+
+def plot_to_base64(fig):
+    buf = BytesIO()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+    return base64.b64encode(buf.read()).decode('utf-8')
+    
+def convert_bytes_to_mb(df, columns):
+    for column in columns:
+        df[column] = df[column] / (1024 * 1024)
+    return df
+
+def generate_stepwise_memory_usage_plots(df, selected_steps):
+    memory_columns = [ 'BytesInUse', 'MaxBytesInUse', 'BytesFromOS']
+    stepwise_plots = {}
+    for step in selected_steps:
+        print(df['StepId'],"----------------------------->")
+        step_data = df[df['StepId'] == step]
+        fig, ax = plt.subplots(figsize=(10, 5))
+        for column in memory_columns:
+            ax.plot(step_data['Baseline'], step_data[column], marker='o', label=column)
+        ax.set_title(f'Step: {step}')
+        ax.set_xlabel('Baseline')
+        ax.set_ylabel('Memory Usage (bytes)')
+        ax.legend()
+        ax.tick_params(axis='x', rotation=20)
+
+        ax.grid(True)
+        stepwise_plots[step] = plot_to_base64(fig)
+        plt.close(fig)
+    return stepwise_plots
+
+
+def generate_stepwise_assembly_state_plots(df, selected_steps):
+    assembly_columns = ['Number of Fully Loaded Parts', 'Number of Partially Loaded Parts', 'Number of Minimally Loaded Parts','Number of OD Parts' ]
+    stepwise_plots = {}
+    for step in selected_steps:
+        step_data = df[df['StepId'] == step]
+        fig, ax = plt.subplots(figsize=(10, 5))
+        for column in assembly_columns:
+            ax.plot(step_data['Baseline'], step_data[column], marker='o', label=column)
+        ax.set_title(f'Step: {step}')
+        ax.set_xlabel('Baseline')
+        ax.set_ylabel('Number of Parts')
+        ax.legend()
+        ax.tick_params(axis='x', rotation=20)
+        ax.grid(True)
+        stepwise_plots[step] = plot_to_base64(fig)
+        plt.close(fig)
+    return stepwise_plots
+
+
+def generate_stepwise_timer_state_plots(df, selected_steps):
+    timer_columns = ['Time required to perform an operation']
+    stepwise_plots = {}
+    for step in selected_steps:
+        step_data = df[df['StepId'] == step]
+        fig, ax = plt.subplots(figsize=(10, 5))
+        for column in timer_columns:
+            ax.plot(step_data['Baseline'], step_data[column], marker='o', label=column)
+        ax.set_title(f'Step: {step}')
+        ax.set_xlabel('Baseline')
+        ax.set_ylabel('Time (seconds)')
+        ax.legend()
+        ax.tick_params(axis='x', rotation=20)
+        ax.grid(True)
+        stepwise_plots[step] = plot_to_base64(fig)
+        plt.close(fig)
+    return stepwise_plots
+
+
+# def generate_stepwise_network_state_plots(df, selected_steps):
+#     network_columns = ['NumSoaCalls', 'NumPdiCalls', 'NumFccCalls']
+#     stepwise_plots = {}
+#     for step in selected_steps:
+#         step_data = df[df['StepId'] == step]
+#         fig, ax = plt.subplots(figsize=(10, 5))
+#         for column in network_columns:
+#             ax.plot(step_data['Baseline'], step_data[column], marker='o', label=column)
+#         ax.set_title(f'Step: {step}')
+#         ax.set_xlabel('Baseline')
+#         ax.set_ylabel('Number of Calls')
+#         ax.legend()
+#         ax.tick_params(axis='x', rotation=20)
+#         ax.grid(True)
+#         stepwise_plots[step] = plot_to_base64(fig)
+#         plt.close(fig)
+#     return stepwise_plots
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def generate_memory_usage_line_plot(df):
+    memory_columns = [
+        'BytesInUse', 'MaxBytesInUse', 'BytesFromOS'
+    ]
+    fig, ax = plt.subplots(figsize=(14, 10))
+    for column in memory_columns:
+        sns.lineplot(data=df, x='StepId', y=column, marker='o', linewidth=2, label=column, ax=ax)
+    ax.set_title('Comparison of Memory Usage Metrics with Respect to StepId', fontsize=16)
+    ax.set_xlabel('StepId', fontsize=14)
+    ax.set_ylabel('Memory Usage (MB)', fontsize=14)
+    ax.legend(title='Memory Metrics', fontsize=12)
+    ax.grid(True, linestyle='--', linewidth=0.5)
+    fig.tight_layout()
+    return plot_to_base64(fig)
+
+def generate_assembly_state_line_plot(df):
+    assembly_columns = [
+        'Number of Fully Loaded Parts', 'Number of Partially Loaded Parts', 'Number of Minimally Loaded Parts','Number of OD Parts'
+    ]
+    fig, ax = plt.subplots(figsize=(14, 10))
+    for column in assembly_columns:
+        sns.lineplot(data=df, x='StepId', y=column, marker='o', linewidth=2, label=column, ax=ax)
+    ax.set_title('Comparison of Assembly State Metrics with Respect to StepId', fontsize=16)
+    ax.set_xlabel('StepId', fontsize=14)
+    ax.set_ylabel('Number of Parts', fontsize=14)
+    ax.legend(title='Assembly State Metrics', fontsize=12)
+    ax.grid(True, linestyle='--', linewidth=0.5)
+    fig.tight_layout()
+    return plot_to_base64(fig)
+
+def generate_timer_state_line_plot(df):
+    timer_columns = ['Time required to perform an operation']
+    fig, ax = plt.subplots(figsize=(14, 10))
+    for column in timer_columns:
+        sns.lineplot(data=df, x='StepId', y=column, marker='o', linewidth=2, label=column, ax=ax)
+    ax.set_title('Comparison of Timer State Metrics with Respect to StepId', fontsize=16)
+    ax.set_xlabel('StepId', fontsize=14)
+    ax.set_ylabel('Time (seconds)', fontsize=14)
+    ax.legend(title='Timer State Metrics', fontsize=12)
+    ax.grid(True, linestyle='--', linewidth=0.5)
+    fig.tight_layout()
+    return plot_to_base64(fig)
+
+# def generate_network_state_line_plot(df):
+#     network_columns = ['NumSoaCalls', 'NumPdiCalls', 'NumFccCalls']
+#     fig, ax = plt.subplots(figsize=(14, 10))
+#     for column in network_columns:
+#         sns.lineplot(data=df, x='StepId', y=column, marker='o', linewidth=2, label=column, ax=ax)
+#     ax.set_title('Comparison of Network State Metrics with Respect to StepId', fontsize=16)
+#     ax.set_xlabel('StepId', fontsize=14)
+#     ax.set_ylabel('Number of Calls', fontsize=14)
+#     ax.legend(title='Network State Metrics', fontsize=12)
+#     ax.grid(True, linestyle='--', linewidth=0.5)
+#     fig.tight_layout()
+#     return plot_to_base64(fig)
+
+
+
+
+
